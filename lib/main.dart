f85727347ce13a808d2bc3eb58301ea7ca92f307
php1301 <luudanthanchet@gmail.com>
2020-09-12 23:41:27 +0700

    Main splitted

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import './question.dart';
-import './answer.dart';
+import './quiz.dart';
+import './result.dart';
 
 void main() => runApp(MyApp());
 
@@ -13,46 +13,78 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  // static const
+
+  final _questionsAndAnswers = const [
+    {
+      'question': 'Color ?',
+      'answers': [
+        {'text': 'Black', 'score': 10},
+        {'text': 'Blue', 'score': 15},
+        {'text': 'Green', 'score': 20},
+        {'text': 'Yellow', 'score': 25},
+      ],
+    },
+    {
+      'question': 'What\'s your favorite Animal ?',
+      'answers': [
+        {'text': 'Bear', 'score': 3},
+        {'text': 'Bird', 'score': 4},
+        {'text': 'Dog', 'score': 5},
+        {'text': 'Cat', 'score': 6},
+      ],
+    },
+    {
+      'question': 'What\'s your favorite Grade ?',
+      'answers': [
+        {'text': 'A', 'score': 1},
+        {'text': 'B', 'score': 1},
+        {'text': 'C', 'score': 1},
+        {'text': 'D', 'score': 1},
+      ],
+    },
+  ];
   var _questionIndex = 0;
-  void _answerQuestion() {
+  var _totalScore = 0;
+  void _resetQuiz() {
+    setState(() {
+      _questionIndex = 0;
+      _totalScore = 0;
+    });
+  }
+
+  void _answerQuestion(int score) {
+    _totalScore += score;
     setState(() {
       _questionIndex += 1;
     });
     print(_questionIndex);
+    if (_questionIndex < _questionsAndAnswers.length) {
+      print('we have more questions');
+    } else {
+      print('Nothing more');
+    }
   }
 
   @override
   Widget build(BuildContext ctx) {
-    const questions = ['Question 1', 'Question 2'];
-    questions.add('2131');
-    var questionsAndAnswers = [
-      {
-        'question': 'Color ?',
-        'answers': ['Black', 'Blue', 'Green', 'Yellow'],
-      },
-      {
-        'question': 'What\'s your favorite Animal ?',
-        'answers': ['Bear', 'Bird', 'Dog', 'Cat'],
-      },
-      {
-        'question': 'What\'s your favorite Grade ?',
-        'answers': ['A', 'B', 'C', 'D'],
-      },
-    ];
+    // const questions = ['Question 1', 'Question 2'];
+    // questions.add('2131');
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Hello Flutter'),
-        ),
-        body: Column(
-          children: [
-            Question(questionsAndAnswers[_questionIndex]['question']),
-            ...(questionsAndAnswers[_questionIndex]['answers'] as List<String>)
-                .map((e) => Answer(_answerQuestion, e))
-                .toList()
-          ],
-        ),
-      ), //ctrl+space
-    );
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text('Hello Flutter'),
+      ),
+      body: _questionIndex < _questionsAndAnswers.length
+          ? Quiz(
+              questionIndex: _questionIndex,
+              answerQuestion: _answerQuestion,
+              questionsAndAnswers: _questionsAndAnswers,
+            )
+          : Result(
+              resultScore: _totalScore,
+              resetQuiz: _resetQuiz,
+            ), //ctrl+space
+    ));
   }
 }
